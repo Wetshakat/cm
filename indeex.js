2f2c31c4fae665ee5fc14827b86c5b314f382190
Ishaku Dyelshak <ishakudyelshak@gmail.com>
2024-11-26 00:48:28 +0100

    Add files via upload

---
+++ indeex.js
@@ -0,0 +1,7 @@
+let bioData ={
+    name :window.prompt("your name"),
+  age :window.prompt("age"),
+ favouriteclub :window.prompt("what is your favourite football team")}
+
+if (bioData.age <18){alert('you must be 18 to access this site')}
+else{alert(`Welcome ${bioData.name} to 90MINUTES Of Banter viewing center,we bring Europe to your doorstep, best of luck to your team`)}
